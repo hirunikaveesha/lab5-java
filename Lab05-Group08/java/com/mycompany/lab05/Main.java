7b08a2a61a0e71c42782a0a03cd3e39ef4b38d07
hirunikaveesha <88107879+hirunikaveesha@users.noreply.github.com>
2022-05-17 12:56:09 +0530

    Add files via upload

---
+++ Lab05-Group08/java/com/mycompany/lab05/Main.java
@@ -0,0 +1,29 @@
+/*
+Group 08 - Members
+IM/2019/004 - Binura Yasith 
+IM/2019/027 - Manod De Silva 
+IM/2019/011 - Nethmi Shihari Wijerama
+IM/2019/018 - Akila Indrasena 
+IM/2019/049 - Ishan Kavindu Umayanga 
+IM/2019/062 - Hiruni Tennakoon
+IM/2019/076 - Visal Nagahawatte 
+IM/2019/095 - Thennila Madhimaaran 
+IM/2019/099 - Thushara Sooriyabandara 
+IM/2019/101 - Hiruni Hathnagoda
+ */
+package com.mycompany.lab05;
+
+/**
+ *
+ * @author acer
+ */
+
+public class Main {
+    public static void main(String[] args) {
+       OpenWindow openWindow=new OpenWindow();
+       openWindow.setVisible(true);
+    }
+}
+
+
+
